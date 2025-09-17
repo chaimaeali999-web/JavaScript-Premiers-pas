8d731e54cf099d7c392520f3f383cf8e6751e804
chaimaeali999-web <chaimaeali999@gmail.com>
2025-09-17 12:55:12 +0100

    11

--- mon projet/script.js
+++ mon projet/script.js
@@ -1,4 +1,4 @@
-let mon project1 = 10 
-let mon project3 = 5
-let somme mon project 1+ mon project3;
+let  project1 = 10 
+let  project3 = 5
+let somme= project1+ project3;
 console.log ("la somme est :"+ somme );
