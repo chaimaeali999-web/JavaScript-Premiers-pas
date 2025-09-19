ed5992615f9b5f981819905d58c934fdbc236e2a
chaymae <chaimaeali999@gmail.com>
2025-09-19 11:04:49 +0100

    condition logique added

---
+++ script.js
@@ -0,0 +1,8 @@
+let age = 20;
+if (age < 18) {
+    console.log("vous étes mineur.");
+} else if (age  === 18) {
+        console.log("vous venez d'atteindre la majorité !");
+    } else {
+        console.log("vou étes majeur.");
+    }
